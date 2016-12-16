fd94be1017f3d1a199b457f851ad9c423717a921
Juan Gallostra Acín <juangallostra@users.noreply.github.com>
2016-12-16 17:01:10 +0100

    Update colourlovers.py

--- colourlovers.py
+++ colourlovers.py
@@ -1,3 +1,7 @@
+import colorsys
+
+
+# Data containers for API responses
 class CommonData(object):
 	def __init__ (self, json_data):
 		self.id = json_data["id"]
@@ -9,6 +13,7 @@ class CommonData(object):
 		self.numhearts = json_data["numHearts"]
 		self.rank = json_data["rank"]
 		self.date_created = json_data["dateCreated"]
+		self.description = json_data["description"]
 		self.url = json_data["url"]
 		self.image_url = json_data["imageUrl"]
 		self.badge_url = json_data["badgeUrl"]
@@ -18,39 +23,68 @@ class CommonData(object):
 
 class Palette(CommonData):
 	def __init__(self,json_data):
-		CommonData.__init__(self,json_data)
+		CommonData.__init__(self, json_data)
 		try:
 			self.color_widths = json_data["colorWidths"]
 		except:
 			pass
-		self.description = json_data["description"]
+		
 		self.colors = json_data["colors"]
 		self.num_colors = len(self.colors)
 
-	def hex_to_rgb(self):	# TODO implement methods
-		pass
+	def hex_to_rgb(self):
+		# converts color in hex to RGB. Returns list of tuples where the channel order is (R,G,B)
+		return [tuple(int(hex_color[i:i+2], 16) for i in (0, 2 ,4)) for hex_color in self.colors]
+
 	def hex_to_hsv(self):
-		pass
+		# converts color in hex to HSV. Returns list of tuples such that each tuple is (H,S,V)
+		return [tuple(colorsys.rgb_to_hsv(rgb_color[0], rgb_color[1], rgb_color[2])) for rgb_color in self.hex_to_rgb()]
+
 
 class Color(CommonData):
 	def __init__(self,json_data):
-		CommonData.__init__(self,json_data)
+		CommonData.__init__(self, json_data)
 		self.hex= json_data["hex"]
-		self.RGB = RGB(json_data["rgb"]) # TODO implement attribute creation
+		self.RGB = RGB(json_data["rgb"]) 
 		self.HSV = HSV(json_data["hsv"])
-		self.description = json_data["description"]
+
 
 class Pattern(CommonData):
 	def __init__(self, json_data):
-		pass
+		CommonData.__init__(self, json_data)
+		self.colors = json_data["colors"]
+
+	def hex_to_rgb(self):	# TODO implement methods
+		# converts color in hex to RGB. Returns list of tuples where the channel order is (R,G,B)
+		return [tuple(int(hex_color[i:i+2], 16) for i in (0, 2 ,4)) for hex_color in self.colors]
+
+	def hex_to_hsv(self):
+		# converts color in hex to HSV. Returns list of tuples such that each tuple is (H,S,V)
+		return [tuple(colorsys.rgb_to_hsv(rgb_color[0], rgb_color[1], rgb_color[2])) for rgb_color in self.hex_to_rgb()]
 
 class Lover(object):
 	def __init__(self, json_data):
-		pass
+		self.id = json_data["id"]
+		self.username = json_data["userName"]
+		self.date_registered = json_data["dateRegistered"]
+		self.date_last_active = json_data["dateLastActive"]
+		self.rating = json_data["rating"]
+		self.location = json_data["location"]
+		self.num_colors = json_data["numColors"]
+		self.num_palettes = json_data["numPalettes"]
+		self.num_patterns = json_data["numPatterns"]
+		self.num_comments_made = json_data["numCommentsMade"]
+		self.num_lovers = json_data["numLovers"]
+		self.num_comments_on_profile = json_data["numCommentsOnProfile"]
+		try:
+			pass
+			# implement comments section
+		except:
+			pass
 
 class Stats(object):
 	def __init__(self, json_data):
-		pass
+		self.total = json_data["total"]
 
 class RGB(object):
 	def __init__(self,rgb):
