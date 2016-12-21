8706993fa436d0fc4a40006782148ceb1cf8491a
Juan Gallostra Acín <juangallostra@users.noreply.github.com>
2016-12-21 17:12:14 +0100

    Update colourlovers_wrapper.py

--- colourlovers_wrapper.py
+++ colourlovers_wrapper.py
@@ -4,11 +4,15 @@
 from urllib2 import Request, urlopen, URLError
 import json
 from colourlovers_data_containers import *
+import collections
 
 # TODOs
-# - implement switches (Lover -> ?comments=1)
-# - implement searches for new, top and random parametres.
-# - valid parameter types when doing unique searches 
+# 	   	- implement switches (Lover -> ?comments=1)
+# DONE 	- implement searches for new, top and random parametres.
+#		- check than when random is used no more parametres are used.
+# 		- valid parameter types when doing unique searches
+# 		- unicode to string conversion ? 
+# 		- stats search
 
 
 # API Wrapper
@@ -17,17 +21,21 @@ class ColourLovers(object):
 	ColourLovers API python wrapper
 	'''
 	def __init__(self):
+
 		self.__API_URL = "http://www.colourlovers.com/api/"
 
-		self.__API_REQUESTS = {"colors":set({"new","top","random"}),
-								 "palettes":set({"new","top","random"}),
-								 "patterns":set({"new","top","random"}),
-								 "lovers":set({"new","top"}),
-								 "stats":set({"colors", "palettes", "patterns", "lovers"}),
-								 "color":set(),
-								 "palette":set(),
-								 "pattern":set(),
-								 "lover":set()}
+		# when searching for new, top or random use the request keyword in the called search method
+		self.__API_REQUEST_KWRD = "request"
+		self.__API_EXCLUSIVE_REQUEST = "random"
+		self.__API_REQUEST_TYPE = {"colors":set({"new","top","random"}),
+									"palettes":set({"new","top","random"}),
+									"patterns":set({"new","top","random"}),
+									"lovers":set({"new","top"}),
+									"stats":set({"colors", "palettes", "patterns", "lovers"}),
+									"color":set(),
+									"palette":set(),
+									"pattern":set(),
+									"lover":set()}
 
 		self.__API_PARAMETRES = {"colors":set({"lover","hueRange","briRange","keywords","keywordExact","orderCol","sortBy","numResults","resultOffset","format","jsonCallback"}),
 								 "palettes":set({"lover","hueOption","hex","hex_logic","keywords","keywordExact","orderCol","sortBy","numResults","resultOffset","format","jsonCallback","showPaletteWidths"}),
@@ -42,7 +50,7 @@ class ColourLovers(object):
 		self.__API_SWITCHES = {"palette":set({"showPaletteWidths"}),
 								"lover":set({"comments"})}
 
-		self.__API_ADD_PARAM = ["&","=","?"]
+		self.__API_ADD_PARAM = ["&","=","?","/"]
 
 		self.__API_COLORS = "colors"
 		self.__API_PALETTES = "palettes" 
@@ -57,10 +65,12 @@ class ColourLovers(object):
 
 	# Public methods
 	def search_colors(self, raw_data=False, **kwargs):
-		if raw_data == False:		# if user hasn't asked for the raw data of the API response build container objects 
-			kwargs["format"] = "json"
+		processed_request =  self.__process_optional_requests(self.__API_COLORS, **kwargs)	
 
-		api_response = self.__search(self.__API_COLORS, **kwargs)
+		if raw_data == False:		# if user hasn't asked for the raw data of the API response build container objects 
+			processed_request.kwargs["format"] = "json"
+
+		api_response = self.__search(self.__API_COLORS, processed_request.optional_request, **processed_request.kwargs)
 
 		containers = self.__process_response(raw_data, api_response, Color) 
 
@@ -74,7 +84,7 @@ class ColourLovers(object):
 		if raw_data == False:		# if user hasn't asked for the raw data of the API response build container objects 
 			kwargs["format"] = "json"
 
-		api_response = self.__search(self.__API_COLOR, **kwargs)
+		api_response = self.__search(self.__API_COLOR, None, **kwargs)
 
 		containers = self.__process_response(raw_data, api_response, Color) 
 
@@ -85,12 +95,12 @@ class ColourLovers(object):
 
 
 	def search_palettes(self, raw_data=False, **kwargs):
+		processed_request =  self.__process_optional_requests(self.__API_PALETTES, **kwargs)
+
 		if raw_data == False:		# if user hasn't asked for the raw data of the API response build container objects 
-			kwargs["format"] = "json"
+			processed_request.kwargs["format"] = "json"
 
-		# handle showPalettesWidth switch
-
-		api_response = self.__search(self.__API_PALETTES, **kwargs)
+		api_response = self.__search(self.__API_PALETTES, processed_request.optional_request, **processed_request.kwargs)
 
 		containers = self.__process_response(raw_data, api_response, Palette) 
 
@@ -104,9 +114,9 @@ class ColourLovers(object):
 		if raw_data == False:		# if user hasn't asked for the raw data of the API response build container objects 
 			kwargs["format"] = "json"
 
-		# handle showPalettesWidth switch
+		# implement switch
 
-		api_response = self.__search(self.__API_PALETTE, **kwargs)
+		api_response = self.__search(self.__API_PALETTE, None, **kwargs)
 
 		containers = self.__process_response(raw_data, api_response, Palette) 
 
@@ -117,10 +127,12 @@ class ColourLovers(object):
 
 
 	def search_patterns(self, raw_data=False, **kwargs):
-		if raw_data == False:		# if user hasn't asked for the raw data of the API response build container objects 
-			kwargs["format"] = "json"
+		processed_request =  self.__process_optional_requests(self.__API_PATTERNS, **kwargs)
 
-		api_response = self.__search(self.__API_PATTERNS, **kwargs)
+		if raw_data == False:		# if user hasn't asked for the raw data of the API response build container objects 
+			processed_request.kwargs["format"] = "json"
+
+		api_response = self.__search(self.__API_PATTERNS, processed_request.optional_request, **processed_request.kwargs)
 
 		containers = self.__process_response(raw_data, api_response, Pattern) 
 
@@ -134,7 +146,7 @@ class ColourLovers(object):
 		if raw_data == False:		# if user hasn't asked for the raw data of the API response build container objects 
 			kwargs["format"] = "json"
 
-		api_response = self.__search(self.__API_PATTERN, **kwargs)
+		api_response = self.__search(self.__API_PATTERN, None, **kwargs)
 
 		containers = self.__process_response(raw_data, api_response, Pattern) 
 
@@ -145,13 +157,12 @@ class ColourLovers(object):
 
 
 	def search_lovers(self, raw_data=False, **kwargs):
+		processed_request =  self.__process_optional_requests(self.__API_LOVERS, **kwargs)
+
 		if raw_data == False:		# if user hasn't asked for the raw data of the API response build container objects 
-			kwargs["format"] = "json"
+			processed_request.kwargs["format"] = "json"
 
-
-		# implement comments switch
-
-		api_response = self.__search(self.__API_LOVERS, **kwargs)
+		api_response = self.__search(self.__API_LOVERS, processed_request.optional_request, **processed_request.kwargs)
 
 		containers = self.__process_response(raw_data, api_response, Lover) 
 
@@ -161,13 +172,13 @@ class ColourLovers(object):
 			print "The data you asked for could not be retrieved"
 
 
-	def search_lover(self, raw_data=False, **kwargs):		
+	def search_lover(self, raw_data=False, **kwargs):
 		if raw_data == False:		# if user hasn't asked for the raw data of the API response build container objects 
 			kwargs["format"] = "json"
 
 		# implement comments switch
 
-		api_response = self.__search(self.__API_LOVER, **kwargs)
+		api_response = self.__search(self.__API_LOVER, None, **kwargs)
 
 		containers = self.__process_response(raw_data, api_response, Lover) 
 
@@ -178,31 +189,35 @@ class ColourLovers(object):
 
 
 	def search_stats(self, raw_data=False, **kwargs):
-		if raw_data == False:		# if user hasn't asked for the raw data of the API response build container objects 
-			kwargs["format"] = "json"
+		processed_request =  self.__process_optional_requests(self.__API_STATS, **kwargs)
 
-		api_response = self.__search(self.__API_STATS, **kwargs)
+		if raw_data == False:		# if user hasn't asked for the raw data of the API response build container objects 
+			processed_request.kwargs["format"] = "json"
+
+		api_response = self.__search(self.__API_STATS, processed_request.optional_request, **processed_request.kwargs)
 
 		containers = self.__process_response(raw_data, api_response, Stats) 
 
 		if containers is not None:
 			return containers
-
 		else:
 			print "The data you asked for could not be retrieved"
 
 
+
+
 	# Private methods
-	def __search(self, searchterm, **kwargs):
+	def __search(self, searchterm, optional_request_term, **kwargs):
 		try: 
 			self.__check_args(searchterm, **kwargs)
 		except ValueError as e:
 			print(e)
 
-		return self.__request(searchterm, **kwargs)
+		return self.__request(searchterm, optional_request_term, **kwargs)
 			
+
 	def __check_args(self, searchterm, **kwargs):
-		if searchterm not in self.__API_REQUESTS.keys():
+		if searchterm not in self.__API_REQUEST_TYPE.keys():
 			raise ValueError("Unsupported search: "+searchterm)
 
 		elif kwargs is not None:
@@ -230,9 +245,14 @@ class ColourLovers(object):
 		else:
 			return True
 
-	def __request(self, searchterm, **kwargs):
+
+
+	def __request(self,searchterm, optional_request_term, **kwargs):
 		# build API request
-		api_request_url = self.__API_URL+searchterm
+		try:
+			api_request_url = self.__API_URL+searchterm+optional_request_term
+		except:
+			api_request_url = self.__API_URL+searchterm
 
 		for argument in kwargs.keys():
 			# build API parameter specification string
@@ -257,15 +277,40 @@ class ColourLovers(object):
 
 
 	def __process_response(self, raw_data, api_response, request_type_class):
-
 		if api_response is not None:
 			if raw_data == True:
 				return api_response
 			else:
 				parsed_json = json.loads(api_response)
-				response_containers = []
-				for element in parsed_json:
-					response_containers+=[request_type_class(element)]
+				if type(parsed_json)==dict:
+					response_containers = request_type_class(parsed_json)
+				else:
+					response_containers = []
+					for element in parsed_json:
+						response_containers+=[request_type_class(element)]
 				return response_containers
 		else:
 			return None
+
+
+	def __process_optional_requests(self, search_type, **kwargs):
+		processed_request = collections.namedtuple('Processed_request',['kwargs','optional_request'])
+
+		optional_request_term = None
+
+		if self.__API_REQUEST_KWRD in kwargs.keys():
+			if type(kwargs[self.__API_REQUEST_KWRD]) == str:
+				request = set({kwargs[self.__API_REQUEST_KWRD]})
+				valid_request =  bool(request.intersection(self.__API_REQUEST_TYPE[search_type]))
+				if valid_request:
+					optional_request_term = self.__API_ADD_PARAM[3]+kwargs[self.__API_REQUEST_KWRD]
+					del kwargs[self.__API_REQUEST_KWRD]
+					if self.__API_EXCLUSIVE_REQUEST in request or search_type == self.__API_STATS: # if the optional request is random/the search is for stats
+																								   # then ignore the rest of arguments since they are not allowed
+						kwargs = {}
+				else:
+					raise ValueError("Unsupported request argument/s: "+ kwargs[self.__API_REQUEST_KWRD])
+			else:
+				raise ValueError("Unsupported request argument type: "+str(type(kwargs[self.__API_REQUEST_KWRD])))
+
+		return processed_request(kwargs=kwargs, optional_request=optional_request_term)
